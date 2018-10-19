e9bbbe073e3020ad2a14e5f978eeb2444b19d084
Siyang-Zhang <siyangz2@illinois.edu>
2018-10-18 19:55:01 -0500

    pca

--- groupproject.py
+++ groupproject.py
@@ -11,6 +11,7 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing
 from sklearn.cross_validation import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 df = pd.read_csv("/Users/siyangzhang/Desktop/课程/ie598 machine learning in finance/groupproject/MLF_GP1_CreditScore.csv")
 
@@ -32,4 +33,79 @@ X_test = scaler.transform(X_test)
 #print summary of data frame
 summary = X.describe()
 print(summary)
+
+
+
+# Principal component analysis in scikit-learn
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    # plot class samples
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.6, 
+                    c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx], 
+                    label=cl)
+
+
+
+pca = PCA()
+X_train_pca = pca.fit_transform(X_train)
+pca.explained_variance_ratio_
+
+
+
+plt.scatter(X_train_pca[:, 0], X_train_pca[:, 1])
+plt.xlabel('PC 1')
+plt.ylabel('PC 2')
+plt.show()
+
+pca.explained_variance_ratio_.shape      
+
+plt.bar(range(0, 26), pca.explained_variance_ratio_, alpha=0.5, align='center')
+plt.step(range(0, 26), np.cumsum(pca.explained_variance_ratio_), where='mid')
+plt.ylabel('Explained variance ratio')
+plt.xlabel('Principal components')
+
+plt.show()
+
+
+
+
+
+pca = PCA(n_components=2)
+X_train_pca = pca.fit_transform(X_train)
+X_test_pca = pca.transform(X_test)
+
+lr = LogisticRegression()
+lr.fit(X_train_pca,y_train)
+print('Training accuracy:', lr.score(X_train_pca, y_train))
+print('Test accuracy:', lr.score(X_test_pca, y_test))
+
+
+
+
+
 
